9dd945953adf0c58467f61ac28f5bb23a6ae025d
franckymint <franckymint@franckymint-MS-7816>
2017-08-14 18:07:25 +0200

    Création de sous-menus "niveau" et "mode de jeu" dans le menu "jeu".

--- MathsExos/src/com/francky/mathsExos/vues/Menu.java
+++ MathsExos/src/com/francky/mathsExos/vues/Menu.java
@@ -19,8 +19,10 @@ public class Menu extends JMenuBar {
 	JMenu menuAffichage, ssmenuThemCoul;
 	JMenuItem itemTheme1, itemTheme2, itemTheme3, itemTheme4, itemTheme5, itemTheme6;
 	
-	JMenu menuJeu;
+	JMenu menuJeu, menuNiveau, menuModeJeu;
 	JMenuItem itemStart, difficulte, itemQuit;
+	JMenuItem itemNiveau1, itemNiveau2, itemNiveau3;
+	JMenuItem itemModeAddition, itemModeSoustraction, itemModeMultiplication, itemModeDivision, itemModeRandom;
 	
 	EcouteurActions menuListener;
 	
@@ -32,7 +34,50 @@ public class Menu extends JMenuBar {
 		menuJeu = new JMenu("Jeu");
 		this.add(menuJeu);
 		
-
+		//menu NIVEAU
+		menuNiveau = new JMenu("Niveau");
+		itemNiveau1 = new JMenuItem("niveau 1");
+		itemNiveau2 = new JMenuItem("niveau 2");
+		itemNiveau3 = new JMenuItem("niveau 3");
+		menuNiveau.add(itemNiveau1);
+		menuNiveau.add(itemNiveau2);
+		menuNiveau.add(itemNiveau3);
+		
+		itemNiveau1.addActionListener(menuListener);
+		itemNiveau1.setActionCommand("NIVEAU1");
+		itemNiveau2.addActionListener(menuListener);
+		itemNiveau2.setActionCommand("NIVEAU2");
+		itemNiveau3.addActionListener(menuListener);
+		itemNiveau3.setActionCommand("NIVEAU3");
+		
+		menuJeu.add(menuNiveau);
+		
+		//menu MODE DE JEU
+		menuModeJeu = new JMenu("mode du jeu");
+		itemModeAddition = new JMenuItem("addition");
+		itemModeSoustraction = new JMenuItem("soustraction");
+		itemModeMultiplication = new JMenuItem("multiplication");
+		itemModeDivision = new JMenuItem("division");
+		itemModeRandom = new JMenuItem("au hasard");
+		menuModeJeu.add(itemModeAddition);
+		menuModeJeu.add(itemModeSoustraction);
+		menuModeJeu.add(itemModeMultiplication);
+		menuModeJeu.add(itemModeDivision);
+		menuModeJeu.add(itemModeRandom);
+		
+		itemModeAddition.addActionListener(menuListener);
+		itemModeAddition.setActionCommand("MODE_ADDITION");
+		itemModeSoustraction.addActionListener(menuListener);
+		itemModeSoustraction.setActionCommand("MODE_SOUSTRACTION");
+		itemModeMultiplication.addActionListener(menuListener);
+		itemModeMultiplication.setActionCommand("MODE_MULTIPLICATION");
+		itemModeDivision.addActionListener(menuListener);
+		itemModeDivision.setActionCommand("MODE_DIVISION");
+		itemModeRandom.addActionListener(menuListener);
+		itemModeRandom.setActionCommand("MODE_HASARD");
+		
+		menuJeu.add(menuModeJeu);
+				
 		itemStart = new JMenuItem("Start / restart");
 		itemQuit = new JMenuItem("Quit");
 		menuJeu.add(itemStart);
